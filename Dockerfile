359242f8c4e0eb0ca61844963a903d0947ce0f5d
Nuno <nuno.ferreira@gamesinc.co.uk>
2018-09-13 17:07:22 +0100

    first commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM golang
+
+WORKDIR /go
+
+COPY ./cards .
+
+RUN go build
+
+CMD ["go", "run", "./main.go" ]
---
+++ cards/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	card := "Ace of Spades"
+
+	fmt.Println(card)
+}
